9abf1b8db367a10e3958ba51dbd0782b53b919fb
shajanjjacob <shajanjjacon@gmail.com>
2023-08-04 20:03:10 +0530

    first commit

---
+++ school/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import User
+from .models import Attendance,StudentExtra,TeacherExtra,Notice
+
+
+class StudentExtraAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(StudentExtra, StudentExtraAdmin)
+
+class TeacherExtraAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(TeacherExtra, TeacherExtraAdmin)
+
+class AttendanceAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Attendance, AttendanceAdmin)
+
+class NoticeAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Notice, NoticeAdmin)
+
+
+
+
+
+
+# Register your models here.
